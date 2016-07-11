34896b7453894eff4c9a887dfddbc4f64cd167bc
Adrian Lemes <adrianlemess@gmail.com>
2016-07-11 14:18:39 -0300

    First Commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Contacts} from 'ionic-native';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage implements OnInit{
+  contacttobefound: string;
+  contacts: any[];
+  search: boolean;
+
+  constructor(private nav: NavController) {
+  }
+  static get parameters(){
+    return [NavController];
+  }
+  ngOnInit() {
+    Contacts.find(['*'],{}).then((contacts) => {
+          this.contacts = contacts;
+          if (this.contacts.length == 0)
+          this.contacts.push({displayName: 'No contacts found'})
+      })
+  }
+ 
+ 
+
+}
