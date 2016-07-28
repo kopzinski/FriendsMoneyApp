db2716b04bb7fd423c3fa9abfa6c409f364a590f
Adrian Lemes <adrianlemess@gmail.com>
2016-07-28 14:10:33 -0300

    Corrigido problema na listagem de pendências

--- www/js/service.contacts.js
+++ www/js/service.contacts.js
@@ -7,7 +7,7 @@ angular.module('starter.service', [])
         var data = {
             contacts : contacts
         }
-         var url = "http://10.96.127.185:3000/api/contacts";
+         var url = "http://10.96.127.155:3000/api/contacts";
 
 
         return $http.post(url, data).then(function(response){
@@ -23,7 +23,7 @@ angular.module('starter.service', [])
                 transaction : transaction,
                 user : user
             }
-            var url = "http://10.96.127.185:3000/api/userTransaction";
+            var url = "http://10.96.127.155:3000/api/userTransaction";
  
             return $http.post(url, data).then(function(response){
                 var responses = response.data;
@@ -33,7 +33,7 @@ angular.module('starter.service', [])
 
         registerTransactionWithFlag:function(transaction){
             var data = transaction;
-            var url = "http://10.96.127.185:3000/api/transaction";
+            var url = "http://10.96.127.155:3000/api/transaction";
            
             return $http.post(url, data).then(function(response){
 
@@ -52,7 +52,7 @@ angular.module('starter.service', [])
         },
         changeStatusPendencie: function(transaction){
 
-            var url = "http://10.96.127.185:3000/api/transaction";
+            var url = "http://10.96.127.155:3000/api/transaction";
             var req = {
                 method: 'PUT',
                 url: url,				   
@@ -77,7 +77,7 @@ angular.module('starter.service', [])
             deviceId: device
         }
 
-         var url = "http://10.96.127.185:3000/api/user";
+         var url = "http://10.96.127.155:3000/api/user";
         return $http.post(url, newUser).then(function(response){
             return response;
         })
