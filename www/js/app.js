059476503048fb0451198784174cda034816c48c
Guilherme Webber <gwm131196@hotmail.com>
2016-10-31 11:59:25 -0200

    terminando de arrumar layout e porta nova

--- www/js/app.js
+++ www/js/app.js
@@ -7,8 +7,7 @@
 
 angular.module('starter', ['ionic.wizard', 'ngMessages','angular.filter','ui.utils.masks', 'ionic', 'starter.controller.contact', 'starter.controller.pendencies', 'ngCordova', 'starter.service','starter.routes', 'starter.Storage','register.controllers', 'starter.controller.timeline', 'starter.controller.groups','starter.service.file']).
 constant('ApiEndpoint', {
-  url: 'http://10.96.127.185:3000/api'
-  //url: 'http://200.169.20.118:8182/api'
+  url: 'http://45.55.243.232:3000/api'
 })
 
 .run(function($ionicPlatform) {
